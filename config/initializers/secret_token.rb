e53e18ee9d8a6fdf692aa14a776192f1cbff304b
Juliana Seng <hexcone@gmail.com>
2014-05-24 04:39:04 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Julianacat::Application.config.secret_key_base = '73783cad81536315f1e5cecbc6cb9d6c7253df1ee1c88cf3ed15c42bec4370a54ebefa6dee78cea6b1499a7da7875b8114ac1235bbdfc244b223519ebbf6cd59'
